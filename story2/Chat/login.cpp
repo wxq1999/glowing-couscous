1925083a7857d5dcf1e9606685e381e321f5b285
Diego369 <xdx374208669@gmail.com>
2019-12-07 00:13:48 +0800

    add GUI

---
+++ story2/Chat/login.cpp
@@ -0,0 +1,44 @@
+﻿#include "login.h"
+#include "ui_login.h"
+
+#include "mainwindow.h"
+
+Login::Login(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Login)
+{
+    ui->setupUi(this);
+    ui->verticalLayout->setContentsMargins(0,0,0,0);
+    ui->horizontalLayout->setContentsMargins(0,0,0,0);
+}
+
+Login::~Login()
+{
+    delete ui;
+}
+
+void Login::on_pushButton_login_clicked()
+{
+    switch(confirm())
+    {
+    case 0:
+        close();
+        MainWindow *w=new MainWindow();
+        w->show();
+        break;
+    }
+}
+
+int Login::confirm()
+{
+    return 0;
+}
+
+void Login::on_pushButton_register_clicked()
+{
+    if(r==nullptr)
+    {
+        r=new Register();
+        r->show();
+    }
+}
---
+++ story2/Chat/mainwindow.cpp
@@ -0,0 +1,46 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //this->setWindowFlags(Qt::FramelessWindowHint);//去掉标题栏
+    //this->setGeometry(QRect(950, 55, 350, 250));//可设置窗口显示的方位与大小
+
+    //this->setWindowOpacity(0.7);//设置透明1-全体透明
+    //this->setAttribute(Qt::WA_TranslucentBackground, true);//设置透明2-窗体标题栏不透明,背景透明
+    //this->resize(300,300);//显示大小
+
+    ui->widget_friend1->Init("Jay Chou");
+    ui->widget_friend2->Init("Ten");
+
+    ui->verticalLayout->setSpacing(0);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_ChatButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_FriendButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->ChatSpace->setCurrentIndex(0);
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->ChatSpace->setCurrentIndex(1);
+}
